dbe57f52b21988009d5e50cfafa874539c8a9722
Rene Hexel <r.hexel@griffith.edu.au>
2026-03-08 11:54:32 +1000

    Use stable swift-ecore dependency release

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
-        .package(url: "https://github.com/mipalgu/swift-ecore", branch: "main"),
+        .package(url: "https://github.com/mipalgu/swift-ecore", from: "0.1.1"),
     ],
     targets: [
         .target(
